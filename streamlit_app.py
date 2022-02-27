b21cf60ccfe6d8d05e5301df5531beb1ec4f0e0e
Tanay Kulkarni <97647504+tanay-gits@users.noreply.github.com>
2022-02-26 20:24:20 -0500

    Update streamlit_app.py
    Final Version dated 2/26/2022

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,12 +44,15 @@ sanitpie_melt = pd.melt(sanitpie, id_vars=['COUNTRY','YEAR'], value_vars=['BASIC
 st.title("UN SDG 6: Clean Water and Sanitation")
 st.subheader("An Exploratory Visualization Application to Find Key Insights")
 st.image("https://blantyre.dorium.community/uploads/default/optimized/1X/6fc93ea6f54ff0312e52bf977c07f91e35efdf40_2_1035x322.jpeg")
-st.write("This is an introduction to the topic")
+st.write("United Nations has gloabally designed several Sustainable Developement Goals(SDGs) as actions to end poverty, protect the planet and ensure peace and prosperity for human beings. SDGs are the extensions of Millenium Developement Goals(MDGs), which were started in the year 2000 to serve the same purpose. SDG-6 is to ensure availability and safe and sustainable management of water and sanitation for all. This project analyzes overall developement of countries around the world, towards safely managing drinking water and sanitation.")
 
 ##WORLD POPULATION SLIDER
 st.header("1. Growth in World Population over Time")
 st.image('https://unstats.un.org/sdgs/assets/img/sliders/2017-Regions-E-large.png')
-st.write("This is an introduction to the topic")
+st.write("The United Nations categorized the world nations in Eight Major Regions, viz.,",
+         "'Sub-Saharan Africa', 'Northern & Western Africa', 'Central & Southern Asia', 'Eastern & South-Eastern Asia'",
+         ", 'Latin America & the Caribbean', 'Australia & New-Zealand','Oceania', and 'Europe & Northern America'.")
+
 slider1 = alt.binding_range(min=2000, max=2020, step=1, name='Select year:')
 select_year1 = alt.selection_single(name="YEAR", fields=['YEAR'],
                                    bind=slider1, init={'YEAR': 2000})
@@ -103,12 +106,15 @@ popgrowth= alt.concat(
 )
 st.altair_chart(popgrowth, use_container_width=True)
 st.caption("Growth in World's Population over Time (2000-2020) (Interactive)")
+st.write("The world population grew exponentially from around 6 Billion in 2000 to about 8 Billion by 2020! This steep rise in population put great stress on the world economies to ensuring clean potable drinking water and safe sanitation to each and every human being on the planet. Population is an important and consistently growing parameter on which, developement of any nation largely depends. This section shows a pair of histograms depicting population growth in different countries and different SDG Regions in the the world between from the year 2000 to 2020. ")
+st.write("**Interactivity Guide:** Move the slider, hover on the bars to view more details...")
+
 
 ## PART A - CLEAN WATER
 st.header("2. Clean Water")
 
 ## THE WATER CORRELATION MATRIX
-st.write("This text introduces the various features and the correlation matrix")
+st.write("The data obtained has 10 different parameters. To visualize the correlation (connection) between these parameters, a correlation matrix is plotted. Many parameters show strong correlation among themselves.")
 
 # THE MATRIX
 cor_data = (waterdf[['BASIC_WAT_NAT', 	'LIMITED_WAT_NAT', 	'UNIMPROVED_WAT_NAT', 	'SURFACE_WAT_NAT', 	'SAFELY_MANAGED_NAT', 	'ACCESS_ONPREMISE_NAT', 	'AVAIL_WHEN_NEEDED_NAT', 	
@@ -140,11 +146,11 @@ cor_plot = base.mark_rect().encode(
 )
 st.altair_chart((cor_plot + text))
 st.caption("Correlation Matrix for Water Feature Data")
-
+st.write("The SDG is to ensure clean drinking water, hence the most important parameter is 'Non Contaminated Water', which shows significantly high (80%) correlation with the 'Piped Water'. This indicates that as the piped water networks increase, the delivery of non-contaminated water increases.")
 
 ## CLASSIFICATION OF DRINKING WATER INFRASTRUCTURE/ METHODS
 st.header("2.1. Classification of Drinking Water Infrastructure/ Methods")
-st.write("This text introduces the topic")
+st.write("From ancient ground/surface water withdrawl to modern pipe networks, methods of access to drinking water are developing continuously. The scatter plot in this section shows increase in population of different countries having access to safe/purified piped water through 20 years. The different dot sizes depict population of a country. ")
 selection = alt.selection_single(fields=['YEAR_STR','COUNTRY'])
 
 pipedwaterchart = alt.Chart(waterdf).mark_circle(opacity=0.9).encode(
@@ -181,11 +187,14 @@ chart_pie = alt.hconcat(
 )
 st.altair_chart(chart_pie)
 st.caption("Increase in Access to Piped Drinking Water (left) and Type of Access to Drinking Water (right) (Interactive)")
+st.write("**Interactivity Guide:** Hover/ Click the 'Country' beads to see the pie change adaptively for the selected Country and Year. To deselect click on whitespace...")
+st.write("As we hover over the graph, the tooltip (cursor) shows name of the country of a particular data point. Single Selection which acts as a dynamic query filter, enables user to click on any point and disaply its details on-demand in the form of a pie chart, alongside. The pie chart shows the accessability to Basic, Limited, Unimproved or Surface water in each country. This gives overall idea of the country's water infrastructure.")
+
 
 ## PERFORMANCE OF COUNTRIES IN DELIVERING NONCONTAMINATED DRINKING WATER
 st.header("2.2. Performance by Nations in Delivering Non-contaminated, Safe Drinking Water to its Citizens")
-st.write("This text introduces the topic")
-slider2 = alt.binding_range(min=2000, max=2020, step=1, name='YEAR')
+st.write("As the goal of the SDG is to provide clean/safe drinking water to all, the scatter plots are created to show World Population vs. Safely Managed Water, Non-Contaminated vs. Non-Piped Water, and Non-Contaminated vs. Piped Water. The different dot sizes depict population of a country. The Slider of Years, help dynamically compare the progress of different nations over the time. ")
+slider2 = alt.binding_range(min=2000, max=2020, step=1, name='Select year:')
 select_year2 = alt.selection_single(name="YEAR", fields=['YEAR'],
                                    bind=slider2, init={'YEAR': 2000})
 
@@ -258,13 +267,14 @@ st.write(alt.concat(
 )
 )
 st.caption("Performance by Nations in Delivering Safely Managed Drinking Water to its Citizens(Interactive)")
-    
+st.write("**Interactivity Guide:** Move the slider to and fro to visualize.")
+st.write("For most of the countries, the parameters in all the three graphs show clear relation. Non-Contaminated water increases as the Safe management of water increases. Non-piped water increases/decreases, Non-contaminated water decreases/increases. Non-contaminated water increases as Pipe water increases.")
 #########################################################################################################################    
     
 ## PART B - SANITATION 
 st.header("3. Sanitation")
 ## THE SANITATION CORRELATION MATRIX
-st.write("This text introduces the topic")
+st.write("Sanitatary waste-water systems have been a tremendously neglected infrastructure, especially in the developing and under-developed countries. The data obtained has 11 different parameters. To visualize the correlation (connection) between these parameters, a correlation matrix is plotted. A couple of parameters show strong correlation among themselves.")
  
 sanit_cor_data = (sanitdf[['BASIC_SAN_NAT', 	'LIMITED_SHARED_SAN_NAT', 	'UNIMPROVED_SAN_NAT', 	'OPENDEFECATION_SAN_NAT', 	'SAFELYMANAGED_SAN_NAT', 	'DISPOSED_INSITU_SAN_NAT', 	'EMP_TREAT_SAN_NAT', 	
                     'WW_TREATED_SAN_NAT', 	'LATRINES_SAN_NAT', 	'SEPTICTANKS_SAN_NAT', 'SEWERCONNECTION_SAN_NAT']]
@@ -293,11 +303,11 @@ sanit_cor_plot = s_base.mark_rect().encode(
 )
 st.write(sanit_cor_plot + text)
 st.caption("Correlation Matrix for Sanitation Feature Data")
-
+st.write("The SDG is to ensure safe management of sanitary waste, hence the most important parameter is 'Safely Managed Sanitary SYstem', which shows significantly high (91%) correlation with the 'Sewer Connection'. This indicates that as the Connections to Sewer Networks increase, the Safe Management of Sewer Waste increases.")
 
 ## CLASSIFICATION OF SANITATION SEWERAGE INFRASTRUCTURE/ METHODS
 st.header("3.1. Classification of Sewerage Infrastructure/ Methods")
-st.write("This text introduces the topic")
+st.write("Although Open-defecation is extremely unhygenic, a large number of world-population rely on it. However, the situation is slowly changing. Most of the countries have underground and safe sewer-systems on their developement agenda. The scatter-plot in this section shows increase in population having Sewerage Connection, over 20 years. The different dot sizes depict population of a country. As we hover over the graph, the tooltip (cursor) shows name of the country of a particular data point. Single Selection which acts as a dynamic query filter, enables user to click on any point and disaply its details on-demand in the form of a pie chart, alongside.")
 
 s_selection = alt.selection_single(fields=['YEAR','COUNTRY'])
 sewerconnectionchart = alt.Chart(sanitdf).mark_circle(opacity=0.9).encode(
@@ -333,11 +343,14 @@ st.write(alt.hconcat(
     stroke=None
 ))
 st.caption("Increase in Underground Sewerage (left) and Type of Disposal of Sanitary Waste (right) (Interactive)")
+st.write("**Interactivity Guide:** Hover/ Click the 'Country' beads to see the pie change adaptively for the selected Country and Year. To deselect click on whitespace...")
+st.write("The pie chart shows classification of Sewerage Infrastructure in Basic, imited-shared, Unimproved Sanition and Open defecation. It gives over-all idea of the country's sewerage infrastructure and availability of safely managed sewerage systems. Most of the countries show significant improvement in 20 years.")
+
 
 ## PERFORMANCE OF COUNTRIES IN DELIVERING NONCONTAMINATED DRINKING WATER
 st.header("3.2. Performance by Nations in Safe Collection and Disposal of Sanitary Wastewater from its Citizens")
-st.write("This text introduces the topic")
-s_slider = alt.binding_range(min=2000, max=2020, step=1, name='YEAR')
+st.write("SDGs aim to irradicate open defecation and provide safely managed sewerage infrastructure to the people. This section contains scatter plots showing Treated Waste-Water vs. Safely Managed Sanitary System. As their names suggest, these are interdependent and most of the countries show relative progress in these two parameters. The scatter-plot of Treated Waste-Water vs. Open-defecation shows that irradicating open-defecation is a slow yet continuously progresssing process. ")
+s_slider = alt.binding_range(min=2000, max=2020, step=1, name='Select year:')
 s_select_year = alt.selection_single(name="YEAR", fields=['YEAR'],
                                    bind=s_slider, init={'YEAR': 2000})
 ## WTSM WW Treated vs. Safely Managed
@@ -386,7 +399,7 @@ WTSC = alt.Chart(sanitdf).mark_circle(opacity=0.9).encode(
 
 s_worldpop = alt.Chart(sanitdf).mark_bar().encode(
     x="YEAR:O",
-    y=alt.Y("sum(POP_THOUS):Q",scale=alt.Scale(domain=(0,8000000))),
+    y=alt.Y("sum(POP_THOUS):Q",scale=alt.Scale(domain=(0,8000000)),axis=alt.Axis(title='World Population (in 1000s)')),
     color=alt.Color('YEAR:N', scale=alt.Scale(scheme='plasma', zero=False)),
     tooltip = 'YEAR'
 ).transform_filter(
@@ -403,8 +416,9 @@ st.write(alt.concat(
 ).configure_view(
     stroke=None
 ))
-    
+st.caption("Performance by Nations in Safe Collection and Disposal of Sanitary Wastewater(Interactive)")
+st.write("**Interactivity Guide:** Move the slider to and fro to visualize.")  
+st.write("Waste-water can be treated only when it is connected to a sewer system, is collected and carried to a treatment plant. The third scatter plot in this section, Treated Waste-Water vs. Sewer Connections show almost direct relation for most of the countries. The different dot sizes depict population of a country. The Slider of Years, help dynamically compare the progress of different nations over the time. For most of the countries, the parameters in all the three graphs show clear relation.")
 
-
-
-st.markdown("This project was created by Tanay Kulkarni and Devashri Karve for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
+st.markdown("***Data Source:** WHO-UNICEF JOINT MONITORING PROGRAM [Webpage](https://washdata.org/how-we-work/sdg-monitoring).*")
+st.markdown("This project was created by [Tanay Kulkarni](https://www.linkedin.com/in/tanaykulkarni/) and [Devashri Karve](https://www.linkedin.com/in/devashrikarve/) for the [Interactive Data Science](https://dig.cmu.edu/ids2022) course at [Carnegie Mellon University](https://www.cmu.edu).")
